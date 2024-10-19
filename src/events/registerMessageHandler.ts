af27c91006093749d65d72d98f99de7f2fd4dc28
Guuzzeji <5lh8dl440f@gmail.com>
2024-10-19 03:53:30 -0700

    Creating register event handler for messages

---
+++ src/events/registerMessageHandler.ts
@@ -0,0 +1,42 @@
+import { Events, ChannelType } from "discord.js";
+
+import { CLIENT } from "../client/index";
+import { getEnvVar } from "../utils";
+
+export function registerMessageHandler() {
+    CLIENT.on(Events.MessageCreate, async msg => {
+        console.log("message", msg.channelId);
+
+        if (msg.author.id === getEnvVar("CLIENT_ID")) {
+
+            // console.log("MESSAGE CONTENTS -->", msg.content);
+            // console.log("MESSAGE CONTENTS -->", msg.attachments);
+
+            if (msg.content.includes("CREATING NEW CHAT")
+                && msg.thread === null
+                && msg.channel.type !== ChannelType.PublicThread) {
+
+                const regexPrompt = /\*\*Prompt\*\*: (.+)/;
+                const matchPrompt = msg.content.match(regexPrompt);
+                const prompt = matchPrompt === null ? "No prompt provided" : matchPrompt[1];
+
+                const regexUsername = /\*\*By\*\*: (.+)/
+                const matchUsername = msg.content.match(regexUsername);
+                const username = matchUsername === null ? "No username provided" : matchUsername[1];
+
+
+                const thread = await msg.startThread({
+                    name: `[AI CHAT] -- ${prompt.slice(0, 20)}...`,
+                    autoArchiveDuration: 60
+                });
+
+                thread.send(`Hello @${username}!`);
+            }
+        }
+
+        if (msg.channel.type === ChannelType.PublicThread && msg.author.id !== getEnvVar("CLIENT_ID")) {
+            console.log("THREAD CONTENTS -->", msg.channel.name);
+            msg.channel.send("We are in a thread now");
+        }
+    });
+}
