148dfc0a73335391027c92a5e7eaa87f2a545330
Guuzzeji <5lh8dl440f@gmail.com>
2024-10-19 03:53:40 -0700

    Create main for starting server

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import 'dotenv/config';
+
+import { startClient } from './src/client/index';
+import { registerCommands } from './src/commands/registerCommands';
+import { registerMessageHandler } from './src/events/registerMessageHandler';
+
+// Run server
+(async () => {
+    startClient();
+    registerMessageHandler()
+    await registerCommands();
+})()
